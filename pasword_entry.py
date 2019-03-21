a429a0e5b679ab976f82157f6980014b31ffa3dd
KaneMontgomery <kane.montgomery@my.jcu.edu.au>
2019-03-21 13:19:12 +1000

    Add Password Check program

--- pasword_entry.py
+++ pasword_entry.py
@@ -1,1 +1,8 @@
 ###Kane Montgomery
+MIN_LEN = 5
+
+password = input("Please enter your password: ")
+if len(password) < MIN_LEN:
+    password = input("Please enter your password: ")
+
+print(len(password) * '*')
